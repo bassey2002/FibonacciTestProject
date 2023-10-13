a880511db5cd51c8afb01dac1f05e58b7c4f4d99
bassey2002 <bassey-godwin.emmanel@binghamuni.edu.ng>
2023-10-13 13:10:51 +0100

    commit

--- src/FibonacciTest.java
+++ src/FibonacciTest.java
@@ -1,11 +1,20 @@
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
-
 public class FibonacciTest {
     @Test
     public void testFirstTen() {
         int[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
-        assertArrayEquals(expected, Fibonacci.getFirstTen());
+        int[] actual = new int[10];
+        actual[0] = 0;
+        actual[1] = 1;
+        for (int i = 2; i < 10; i++) {
+            actual[i] = actual[i - 1] + actual[i - 2];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Fibonacci[" + i + "] = " + actual[i]);
+        }
+
+        assertArrayEquals(expected, actual);
     }
 }
